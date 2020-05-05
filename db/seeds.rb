37c8210e6f59e8bddc518c72085b3ea52c0f1d9a
github username <github email address>
2020-05-05 18:04:59 -0500

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,33 @@
-Favorite.destroy_all
-User.destroy_all
-Recipe.destroy_all
+require 'pry'
+
+Drink.destroy_all
+
+require 'httparty'
+require 'json'
+
+
+# response = HTTParty.get("cocktails.json")
+ # binding.pry
+# response[" "].each do |d|
+ #d1 = Drink.create(name: d["name"], taste: d["taste"])
+ # end
+
+#$cocktails.each do |d|  d1 = Drink.create(name: d["name"], taste: d["taste"]) 
+#
+
+file = File.read('cocktails.json')
+data_hash = JSON.parse(file)
+
+data_hash.each do |d| d1 = Drink.create(name: d["name"], description: d["taste"]) 
+end
+
+data_hash.each do |r| r1= Recipe.create(name: d["name"], ])
+
+
+
+
+
+
+
+
+
