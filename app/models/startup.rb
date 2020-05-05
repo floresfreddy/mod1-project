6904cfbccd9674b2313bc5fdc99c31e56abd4154
floresfreddy <floresfreddy.ultra@gmail.com>
2020-05-05 15:37:36 -0500

    adding json data

---
+++ app/models/startup.rb
@@ -0,0 +1,12 @@
+# This is where the program starts and gives user options
+def startup
+
+    # display project name
+  
+  
+    # ask if you have been here before, if yes put in name, if no create new user
+  
+  
+    # give options
+  
+  end
