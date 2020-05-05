7d319a24d12ffb47de347cb53660867ce0c84703
github username <github email address>
2020-05-05 16:54:02 -0500

    update

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,11 +1,7 @@
 class Drink < ActiveRecord::Base
-<<<<<<< HEAD:app/models/drink.rb
-    has_many :recipies
-    has_many :ingredients, through: :recipies
-=======
     has_many :recipes
     has_many :ingredients, through: :recipes
->>>>>>> yannickdjoumbi:app/models/drink.rb
+    
 
     # def reviews
     #     Review.all.select{|review| review.bakery_id == self.id}
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,28 @@
 require 'pry'
+
 Drink.destroy_all
 
 require 'httparty'
 require 'json'
 
 
-# response = HTTParty.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-# #binding.pry
-# response["drinks"].each do |d|
-# d1 = Drink.create(name: d["strDrink"], description: d["strInstructions"])
-# end
+# response = HTTParty.get("cocktails.json")
+ # binding.pry
+# response[" "].each do |d|
+ #d1 = Drink.create(name: d["name"], taste: d["taste"])
+ # end
+
+#$cocktails.each do |d|  d1 = Drink.create(name: d["name"], taste: d["taste"]) 
+#end
+
+file = File.read('cocktails.json')
+data_hash = JSON.parse(file)
+data_hash.each do |d| d1 = Drink.create(name: d["name"], description: d["taste"]) 
+end
+
+
+
+
 
 
 
